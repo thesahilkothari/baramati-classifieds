59b228fe5b26774c64eaa9d74e9bf50aab910cf5
thesahilkothari <thesahilkothari@gmail.com>
2026-05-28 10:57:20 +0530

    feat: add slugify utility to generate URL-friendly slugs

---
+++ app/lib/slugify.js
@@ -0,0 +1,9 @@
+export function slugify(text) {
+  return text
+    .toString()
+    .toLowerCase()
+    .trim()
+    .replace(/&/g, "and")
+    .replace(/[\s\W-]+/g, "-")
+    .replace(/^-+|-+$/g, "");
+}
